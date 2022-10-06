743458cc17e35d3769246d252e2e04998468631c
fortemp <gnl5672@naver.com>
2022-10-06 20:53:05 +0900

    추가 파이어베이스 스토리지 데이터 조회 함수

--- src/firebase.js
+++ src/firebase.js
@@ -13,7 +13,7 @@ const db = firebase.firestore();
  * @param {string} collection Firestore에 저장된 collection 이름
  *  
  */
-export function testFunction(collection){
+function testFunction(collection){
     db.collection(collection).get().then((querySnapshot) => {
         querySnapshot.forEach((doc)=> {
             console.log(`${doc.id} => ${doc.data()}`);
@@ -28,8 +28,8 @@ export function testFunction(collection){
  * @param {ForFirebase} data collection에 추가할 데이터
  */
 function addData(collection, document, data){
-    console.log(data);
-    db.collection(collection).doc(document)
+    db.collection(collection)
+      .doc(document)
       .withConverter(data.getConvertor)
       .set(data)
       .then((docRef)=>{
@@ -39,3 +39,30 @@ function addData(collection, document, data){
         console.log(`실패\n${error}`)
       });
 }
+
+/**
+ * 파이어베이스 스토리지에 데이터를 조회하는 함수
+ * @param {string} collection Firestore에 저장된 collection 이름
+ * @param {string} document Firestore에 위치한 document 이름
+ * @param {ForFirebase} type 조회할 문서의 데이터 타입 
+ */
+function getData(collection, document, type){
+    db.collection(collection)
+      .doc(document)
+      .withConverter(type.getConvertor)
+      .get()
+      .then((doc) => {
+        if(doc.exists){
+            let data = doc.data();
+            console.log(data);
+        }
+        else {
+            console.log(`문서를 찾을 수 없음`);
+        }
+      })
+      .catch((error) => {
+        console.log(`문서를 가져오는데 에러가 있었습니다.\n${error}`);
+      })
+};
+
+export {testFunction, addData, getData};
