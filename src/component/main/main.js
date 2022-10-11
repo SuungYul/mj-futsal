b14a939cf4e061a81bbf0adc765a7c25c1ff2905
SuungYul <pmtf00@gmail.com>
2022-10-11 19:35:17 +0900

    신청 탭

--- src/component/main/main.js
+++ src/component/main/main.js
@@ -7,27 +7,27 @@ import "firebase/auth";
 import { useState, useEffect } from "react";
 
 const Main = () =>{ 
-    // const navigate = useNavigate();
-    // const [init, setInit] = useState(false); //로그인 상태 
-    // const [isLoggedIn, setIsLoggedIn] = useState(false); //로그인 상태를 관리
-    // useEffect( () => {firebase.auth().onAuthStateChanged((user) => {
-    //             if (user) { //로그인 되어 있을 경우
-    //                 setIsLoggedIn(true);
-    //                 console.log(isLoggedIn);
-    //             }
-    //             else{
-    //                 setIsLoggedIn(false);
-    //             }
-    //             setInit(true)
-    //         }) 
-    // }, []);
+    const navigate = useNavigate();
+    const [init, setInit] = useState(false); //로그인 상태 
+    const [isLoggedIn, setIsLoggedIn] = useState(false); //로그인 상태를 관리
+    useEffect( () => {firebase.auth().onAuthStateChanged((user) => {
+                if (user) { //로그인 되어 있을 경우
+                    setIsLoggedIn(true);
+                    console.log(isLoggedIn);
+                }
+                else{
+                    setIsLoggedIn(false);
+                }
+                setInit(true)
+            }) 
+    }, []);
     return (
         <>
         {<div>
-                {/* <Login/>
-                <LookUp/> */}
-                {/* <div>hi</div> */}
-                <Reserve/>
+                <Login/>
+                <LookUp/>
+                
+                {/* <Reserve/> */}
             </div>
         }
         </>);
