f0cc437b8915b06163ff7c676ca76430f9cefb4d
wu-seong <jws001228@naver.com>
2022-10-12 23:35:05 +0900

    app.js에 로그인상태 및 로그인 대기 상태에 따라 Route 관리

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React from "react"
 import "./App.css"
 import { Routes, Route, Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 import Login from "./component/login.js"
 import Sign from "./component/signUp.js"
 import Main from "./component/main/main"
 import Reserve from "./reservation/reserve";
 import MyPage from "./component/myPage";
+import firebase from "firebase";
+import "firebase/auth"
 
 
 function App() {
-
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [init, setInit] = useState(false)
+  useEffect(firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      setIsLoggedIn(true);
+    } else {
+      setIsLoggedIn(false);
+    }
+    setInit(true);
+  }), [])
   return (
     <div className="App">
-    <Routes>
     
-          
+    {init? 
+    <Routes>
       <Route path="/login" element={<Login/>} />
       <Route path="/signUp" element={<Sign/>} />
       <Route path="/" element={<Main/>} />
-      <Route path="/reserve" element={<Reserve/>}/>
-      <Route path="/mypage" element={<MyPage/>}/>
+      <Route path="/reserve" element={isLoggedIn?<Reserve/> : <Login/>} />
+      <Route path="/mypage" element={isLoggedIn?<MyPage/> : <Login/>} />
     </Routes>
+    : "Initializing..."
+    }
     </div>
   );
 }
