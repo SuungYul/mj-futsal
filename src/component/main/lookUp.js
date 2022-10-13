e9a425627f84f99a8566b6cf53f247773c0a7029
SuungYul <pmtf00@gmail.com>
2022-10-13 21:47:49 +0900

    메인페이지 css 하는중-SY

--- src/component/main/lookUp.js
+++ src/component/main/lookUp.js
@@ -1,17 +1,16 @@
-import {getData} from "../../database/firebase.js"
 import "./lookUp.css"
 const LookUp = () =>{ 
     
     return  <div id = "container">
-        <h2>풋살장 예약 현황 조회</h2>
+        <h2 id="title">풋살장 예약 현황 조회</h2>
         <div className="weekContainer">
-            <div className="week">월</div>
-            <div className="week">화</div>
-            <div className="week">수</div>
-            <div className="week">목</div>
-            <div className="week">금</div>
-            <div className="week">토</div>
-            <div className="week">일</div>
+            <button className="mon">월</button>
+            <button className="tue">화</button>
+            <button className="wed">수</button>
+            <button className="thu">목</button>
+            <button className="fri">금</button>
+            <button className="sat">토</button>
+            <button className="sun">일</button>
         </div>
         <div id ="timeTable">
             <div className="reInfo">17:00 ~ 17:50</div>
@@ -22,3 +21,7 @@ const LookUp = () =>{
     </div>;
 }
 export default LookUp
+
+function buttonlistener () {
+    
+}
