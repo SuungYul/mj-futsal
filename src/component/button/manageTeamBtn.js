e524bfef4ff9641665e06dcb0d89a432ba201fcd
SuungYul <pmtf00@gmail.com>
2022-10-27 22:38:17 +0900

    신청부분 라디오 수정

--- src/component/button/manageTeamBtn.js
+++ src/component/button/manageTeamBtn.js
@@ -2,13 +2,14 @@ import firebase from "firebase/app";
 import {useNavigate} from "react-router-dom"
 import "firebase/auth";
 import ManageTeam from "../team/manageTeam";
+import "./teambutton.css"
 
 const ManageTeamBtn = () =>{
     const navigate = useNavigate();
     const toCreateTeam = () =>{
         navigate("/manage-team");
     }
-    return <button onClick={toCreateTeam}>팀 관리</button>
+    return <button id="manageBtn"onClick={toCreateTeam}>팀 관리</button>
 }
 
 export default ManageTeamBtn
--- src/component/reservation/reserve.js
+++ src/component/reservation/reserve.js
@@ -1,61 +1,95 @@
 import React, { useState } from "react";
 import { useLocation } from "react-router-dom";
 import { ReserveInfo } from "../../database/ReserveInfo";
-
 import "./reserve.css"
+
 const Reserve = (props) => {
-    const {reserveInfo} = useLocation();
+    const { reserveInfo } = useLocation();
     //reserveInfo&&console.log(reserveInfo);
-    const [isTeam, teamCheck] = useState([]);
+    const [isTeam, teamCheck] = useState(false);
+    const [radio_click, setRadio] = useState(true);
     const clickRB = (e) => {
-        teamCheck(e.target.value)
+        if (e.target.id === "team") {
+            teamCheck(true)
+        }
+        else{
+            teamCheck(false)
+        }
     }
     // const reInfo = new ReserveInfo();
-
+    const radioActive = (event) => {
+        if (event.target.id === "play_other") {
+            setRadio(false)
+            teamCheck(false)
+        }
+        if (event.target.id === "play_team") {
+            setRadio(true)
+            teamCheck(true)
+        }
+        // setRadio()
+    }
     return (
-        <div>
-            <h1>풋살장 예약 신청</h1>
-            {/* 현재 예약 정보는 예약 DB에서 긁어와야됨 */}
-            <div> 현재 예약 정보 </div>
-            <div>
-                {props.day} | {props.time}
+        <div id="top_div">
+            <div className="frame">
+                <h1>풋살장 예약 신청</h1>
+                {/* 현재 예약 정보는 예약 DB에서 긁어와야됨 */}
+                <div> 현재 예약 정보 </div>
+                <div>
+                    {props.day} | {props.time}
+                </div>
+                <div>
+                    <input
+                        type="radio"
+                        id="play_other"
+                        name="play_check"
+                        // value="0"
+                        onChange={radioActive}>
+                    </input>
+                    <label htmlFor="play_other">다른 팀과 같이 찰래요</label>
+                    |
+                    <input
+                        type="radio"
+                        id="play_team"
+                        name="play_check"
+                        // value="1"
+                        onChange={radioActive}>
+                    </input>
+                    <label htmlFor="play_team">우리끼리만 찰래요</label>
+                    <br />
+                    <input
+                        type="radio"
+                        id="individual"
+                        name="ck_team"
+                        value={false}
+                        disabled={radio_click}
+                        // checked={isTeam === fa}
+                        onChange={clickRB}
+                    >
+                    </input>
+                    <label htmlFor="individual">개인</label> |
+                    <input
+                        type="radio"
+                        id="team"
+                        name="ck_team"
+                        value={true}
+                        disabled={radio_click && !isTeam}
+                        onChange={clickRB}
+                        checked={isTeam}
+                        
+                    >
+                    </input>
+                    <label htmlFor="team">팀</label>
+                </div>
             </div>
             <div>
-                <input type="checkbox" id="match" name="match"></input>
-                <label htmlFor="match">(선택) 매칭</label>
-                <br />
-                <input
-                    type="radio"
-                    id="ck_team"
-                    name="individual"
-                    value="0"
-                    checked={isTeam === "0"}
-                    onChange={clickRB}>
-                </input>
-                <label htmlFor="individual">개인</label> |
-                <input
-                    type="radio"
-                    id="ck_team"
-                    name="team"
-                    value="1"
-                    checked={isTeam === "1"}
-                    onChange={clickRB}>
-                </input>
-                <label htmlFor="team">팀</label>
-            </div>
-            <div>
-                <article className={(isTeam === "1") ? "art_team" : "art_indi"}>
+                <article className={(isTeam === true) ? "art_team" : "art_indi"}>
                     <h2>팀 명단 작성</h2>
                     {/* 팀 DB 구현되면 작성 */}
                 </article>
             </div>
         </div>
-
-
-
     );
-
-
 }
 
 export default Reserve
+
